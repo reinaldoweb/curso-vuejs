c1fb1582dac7407873ef083490297b3454e19092
Reinaldo <reinaldoweb@hotmail.com>
2021-03-10 09:35:56 -0300

    Carregando noticias com getters

--- hcode-futebol-clube/src/components/HcodeSection.vue
+++ hcode-futebol-clube/src/components/HcodeSection.vue
@@ -11,7 +11,7 @@
 
     <div class="container">
       <div class="row my-club mt-5">
-        <div class="col-6">Seu clube é: {{ clubName }}</div>
+        <div class="col-6">Seu clube é: {{ ClubName }}</div>
         <div class="col-6">
           <h2><HcodeInput v-model="myClub" /></h2>
         </div>
@@ -23,7 +23,7 @@
 <script>
 import HcodeSectionBanner from "./HcodeSectionBanner";
 import HcodeInput from "./HcodeInput";
-import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
@@ -40,10 +40,12 @@ export default {
     currentComponent: String,
   },
   computed: {
-    ...mapState(['championship']),
-    ...mapState({
-      myClub: 'clubName',
-      })
+
+    ...mapGetters({
+      championship: 'getChampionship',
+      myClube: 'getClubName'
+    })
+
   }
   }
 </script>
